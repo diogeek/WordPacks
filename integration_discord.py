d4388bdda58584da46b7cfb1f15c8909530bac01
Dionys <78198189+diogeek@users.noreply.github.com>
2022-03-24 16:50:12 +0100

    iencli.run

--- integration_discord.py
+++ integration_discord.py
@@ -1,24 +1,28 @@
 import discord
 import main
 
-class leClient(discord.Client(activity=discord.Game(name='!help pour les commandes'))):
-    def __init__(self):
-        self.trade_en_cours=False
-        self.proposition_trade=False
-        self.dresseur1_trade=""
-        self.dresseur2_trade=""
-        self.dresseur1_mot=""
-        self.dresseur2_mot=""
-        self.dresseur1_complete=False
-        self.dresseur2_complete=False
-    async def on_ready(self):
-        print(f'Logged in as {self.user.name} - {self.user.id}')
-    async def on_message(self.message):
-        if message.author.id == self.user.id :#empêcher que le bot ne détecte ses propres messages
-            return None
+iencli = discord.Client()
 
-        elif message.content=="!help":
-            await message.channel.send("**Commandes du bot Wordpacks**\n\n\
+trade_en_cours=False
+proposition_trade=False
+dresseur1_trade=""
+dresseur2_trade=""
+dresseur1_mot=""
+dresseur2_mot=""
+dresseur1_complete=False
+dresseur2_complete=False
+
+@iencli.event
+async def on_ready(self):
+    print(f'Logged in as {user.name} - {user.id}')
+
+@iencli.event
+async def on_message(message):
+    if message.author.id == user.id :#empêcher que le bot ne détecte ses propres messages
+        return None
+
+    elif message.content=="!help":
+        await message.channel.send("**Commandes du bot Wordpacks**\n\n\
 ```!help``` - afficher cette page\n\
 ```!kukujariv``` - s'inscrire en tant que dresseur\n\
 ```!mokedex``` - afficher les mots que vous possédez\n\
@@ -28,45 +32,43 @@ class leClient(discord.Client(activity=discord.Game(name='!help pour les command
 ```!refuser``` - refuser la proposition d'échange\n\
 ```!annuler``` - annuler l'échange\n\
 ```!completer``` - compléter l'échange")
-        elif self.proposition_trade and message.author.id==dresseur2_trade and message.content=="!accepter":
-            self.proposition_trade=False
-            self.trade_en_cours=True
-            await message.channel.send(f"Échange entre @{dresseur1} et @{dresseur2} commencé !\n\
+    elif proposition_trade and message.author.id==dresseur2_trade and message.content=="!accepter":
+        proposition_trade=False
+        trade_en_cours=True
+        await message.channel.send(f"Échange entre @{dresseur1} et @{dresseur2} commencé !\n\
 entrez le mot que vous souhaitez échanger et utilisez tous les deux la commande ```!completer``` pour compléter.")
 
-        elif self.proposition_trade and message.author.id==dresseur2_trade and message.content=="!refuser":
-            self.proposition_trade=False
-            await message.channel.send("Échange refusé.")
+    elif proposition_trade and message.author.id==dresseur2_trade and message.content=="!refuser":
+        proposition_trade=False
+        await message.channel.send("Échange refusé.")
 
-        elif self.trade_en_cours and message.author.id in [dresseur2_trade,dresseur1_trade] and message.content=="!annuler":
-            self.trade_en_cours=False
-            await message.channel.send("Échange annulé.")
-            
-        elif message.content=="!kukujariv":
-            main.creer_dresseur(str(message.author.id))
-            await message.channel.send(f"Dresseur @{message.author.id} créé ! Attrapez les tous ! :D")
-
-        elif message.content=="!mokedex":
-            main.afficher_mots(str(message.author.id))
-            #await message.channel.send(f"Le dresseur @{message.author.id} possède les mots suivants : **{(', '.join([raw[0] for raw in record]))}**") CHANGER CA
-
-        elif message.content=="!booster":
-            main.ouverture_booster(str(message.author.id))
-            await message.channel.send(f"Bravo @{message.author.id} ! tu obtiens les mots suivants : **{(', '.join([raw[0] for raw in record]))}**")
-
-        elif message.content.startswith("!echange "):
-            dresseur1_trade=message.author.id
-            dresseur2_trade=message.content.split(" ")[1]
-            if message.guild.get_member(dresseur2_trade) is not None: await message.channel.send(f"Le dresseur @{dresseur1_trade} propose un **échange** avec le dresseur @{dresseur2_trade} !\
-@{dresseur2_trade}, utilisez la commandes ```!accepter``` ou ```!refuser```.")
-            else : await message.channel.send(f"Dresseur '{dresseur2_trade}' introuvable.")
-
-        #elif message.content.startswith("!mokedex"):
-            #main.afficher_mots(str(message.author.id))
-            #await message.channel.send(f"Le dresseur @{message.author.id} possède les mots suivants : {(', '.join([raw[0] for raw in record]))}")
-
+    elif trade_en_cours and message.author.id in [dresseur2_trade,dresseur1_trade] and message.content=="!annuler":
+        trade_en_cours=False
+        await message.channel.send("Échange annulé.")
         
-iencli=leClient()
-iencli.run('token')
+    elif message.content=="!kukujariv":
+        main.creer_dresseur(str(message.author.id))
+        await message.channel.send(f"Dresseur @{message.author.id} créé ! Attrapez les tous ! :D")
+
+    elif message.content=="!mokedex":
+        main.afficher_mots(str(message.author.id))
+        #await message.channel.send(f"Le dresseur @{message.author.id} possède les mots suivants : **{(', '.join([raw[0] for raw in record]))}**") CHANGER CA
+
+    elif message.content=="!booster":
+        main.ouverture_booster(str(message.author.id))
+        await message.channel.send(f"Bravo @{message.author.id} ! tu obtiens les mots suivants : **{(', '.join([raw[0] for raw in record]))}**")
+
+    elif message.content.startswith("!echange "):
+        dresseur1_trade=message.author.id
+        dresseur2_trade=message.content.split(" ")[1]
+        if message.guild.get_member(dresseur2_trade) is not None: await message.channel.send(f"Le dresseur @{dresseur1_trade} propose un **échange** avec le dresseur @{dresseur2_trade} !\
+@{dresseur2_trade}, utilisez la commandes ```!accepter``` ou ```!refuser```.")
+        else : await message.channel.send(f"Dresseur '{dresseur2_trade}' introuvable.")
+
+    #elif message.content.startswith("!mokedex"):
+        #main.afficher_mots(str(message.author.id))
+        #await message.channel.send(f"Le dresseur @{message.author.id} possède les mots suivants : {(', '.join([raw[0] for raw in record]))}")
+
+iencli.run('ba4096d46aa2f237feca511cd518260010a92504d2ec588a5711b1f562de2aa2')
     
 print(f"mots {(', '.join([mot for mot in mots]))} capturés.")
