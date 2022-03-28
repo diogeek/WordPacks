7a57082a526f62dadac3227b8133cc91cb453085
Dionys <78198189+diogeek@users.noreply.github.com>
2022-03-28 16:03:56 +0000

    encore mieux, l'éhange cest pas fini mais ua un coodlwon

--- integration_discord.py
+++ integration_discord.py
@@ -15,75 +15,97 @@ dresseur2_trade=""
 dresseur1_mot=""
 dresseur2_mot=""
 half_complete=False
+suppression=""
 
 prefix="!"
 
 @iencli.event
 async def on_ready():
     print(f'Logged in as {iencli.user.name} - {iencli.user.id}')
+    await iencli.change_presence(activity=discord.Game(name="!help pour les commandes"))
 
 @iencli.event
 async def on_message(message):
-    global trade_en_cours,proposition_trade,dresseur1_trade,dresseur2_trade,dresseur1_mot,dresseur2_mot,half_complete
+    global trade_en_cours,proposition_trade,dresseur1_trade,dresseur2_trade,dresseur1_mot,dresseur2_mot,half_complete,suppression
     if message.author.id == iencli.user.id :#empêcher que le bot ne détecte ses propres messages
         return None
 
     elif message.content==f"{prefix}help":
         await message.channel.send(f"**Commandes du bot Wordpacks**\n\n\
-```{prefix}help``` - afficher cette page\n\
-```{prefix}kukujariv``` - s'inscrire en tant que dresseur\n\
-```{prefix}mokedex``` - afficher les mots que vous possédez\n\
-```{prefix}booster``` - ouvrir un booster de 3 mots\n\
-```{prefix}echange [dresseur]``` - proposer un échange avec un dresseur\n\
-```{prefix}accepter``` - accepter la proposition d'échange\n\
-```{prefix}refuser``` - refuser la proposition d'échange\n\
-```{prefix}annuler``` - annuler l'échange\n\
-```{prefix}completer``` - compléter l'échange")
-    elif proposition_trade and message.author.id==dresseur2_trade and message.content=="{prefix}accepter":
-        proposition_trade=False
-        trade_en_cours=True
-        await message.channel.send(f"Échange entre <@{dresseur1_trade}> et <@{dresseur2_trade}> commencé !\n\
-entrez le mot que vous souhaitez échanger et utilisez tous les deux la commande ```{prefix}completer``` pour compléter.")
-
-    elif proposition_trade and message.author.id==dresseur2_trade and message.content=="{prefix}refuser":
-        proposition_trade=False
-        await message.channel.send("Échange refusé.")
-
-    elif trade_en_cours and message.author.id in [dresseur2_trade,dresseur1_trade] and message.content=="{prefix}annuler":
-        trade_en_cours=False
-        await message.channel.send("Échange annulé.")
-        
-    elif message.content=="{prefix}kukujariv":
+`{prefix}help` - afficher cette page\n\n\
+_Commandes principales_\n\n\
+`{prefix}kukujariv` - s'inscrire en tant que dresseur\n\
+`{prefix}mokedex` - afficher les mots que vous possédez\n\
+`{prefix}booster` - ouvrir un booster de 3 mots\n\
+`{prefix}quitter` - supprimer votre profil de dresseur ainsi que tous vos mots, **définitivement**. Une confirmation est obligatoire\n\
+`{prefix}echange [dresseur]` - proposer un échange avec un dresseur\n\n\
+_Commandes relatives à l'échange_\n\n\
+`{prefix}accepter` - accepter la proposition d'échange\n\
+`{prefix}refuser` - refuser la proposition d'échange\n\
+`{prefix}annuler` - annuler l'échange\n\
+`{prefix}completer` - compléter l'échange")
+    if not main.check_dresseur_existe(message.author.id) and message.content==f"{prefix}kukujariv":
         main.creer_dresseur(str(message.author.id))
         await message.channel.send(f"Dresseur <@{message.author.id}> créé ! Attrapez les tous !")
+    elif main.check_dresseur_existe(message.author.id):
+        if proposition_trade and message.author.id==dresseur2_trade and message.content==f"{prefix}accepter":
+            proposition_trade=False
+            trade_en_cours=True
+            await message.channel.send(f"Échange entre <@{dresseur1_trade}> et <@{dresseur2_trade}> commencé !\n\
+entrez le mot que vous souhaitez échanger et utilisez tous les deux la commande `{prefix}completer` pour compléter.")
 
-    elif message.content=="{prefix}mokedex":
-        record=main.afficher_mots(str(message.author.id))
-        await message.channel.send(f"Le dresseur <@{message.author.id}> possède les mots suivants : **{(', '.join([raw[0] for raw in record]))}**")
+        elif proposition_trade and message.author.id==dresseur2_trade and message.content==f"{prefix}refuser":
+            proposition_trade=False
+            await message.channel.send("Échange refusé.")
 
-    elif message.content=="{prefix}booster":
-        record=main.ouverture_booster(str(message.author.id))
-        print(record)
-        await message.channel.send(f"Bravo <@{message.author.id}> ! tu obtiens les mots suivants : **{(', '.join(record))}**")
+        elif ((trade_en_cours and message.author.id in [dresseur2_trade,dresseur1_trade]) or (proposition_trade and message.author.id==dresseur1_trade)) and message.content==f"{prefix}annuler":
+            trade_en_cours=False
+            await message.channel.send("Échange annulé.")
+          
+        elif message.content==f"{prefix}mokedex":
+            record=main.afficher_mots(str(message.author.id))
+            await message.channel.send(f"Le dresseur <@{message.author.id}> possède les mots suivants : **{(', '.join([raw[0] for raw in record]))}**")
 
-    elif message.content.startswith("{prefix}echange ") and message.content.split(" ")[1]!=message.content.split(" ")[0]:
-        dresseur1_trade=message.author.id
-        dresseur2_trade=message.content.split(" ")[1]
-        if message.guild.get_member(dresseur2_trade) is not None: await message.channel.send(f"Le dresseur <@{dresseur1_trade}> propose un **échange** avec le dresseur <@{dresseur2_trade}> !\
-<@{dresseur2_trade}>, utilisez la commandes ```{prefix}accepter``` ou ```{prefix}refuser```.")
-        else : await message.channel.send(f"Dresseur '{dresseur2_trade}' introuvable.")
+        elif message.content==f"{prefix}booster":
+          if main.cooldown_ready(message.author.id)[0]:
+            record=main.ouverture_booster(message.author.id)
+            await message.channel.send(f"Bravo <@{message.author.id}> ! tu obtiens les mots suivants : **{(', '.join(record))}**")
+          else :
+            await message.channel.send(f"Désolé <@{message.author.id}>, réessaie dans {main.cooldown_ready(message.author.id)[0]}")
 
-    elif trade_en_cours and message.author.id in [dresseur1_trade,dresseur2_trade] and [dresseur1_mot,dresseur2_mot][[dresseur1_trade,dresseur2_trade].index(message.author.id)]!="" and message.content=="{prefix}completer":
-        await message.channel.send(f"Le dresseur <@{message.author.id}> bloque le mot '{[dresseur1_mot,dresseur2_mot][[dresseur1_trade,dresseur2_trade].index(message.author.id)]}'.")
-        if half_complete:
-            await message.channel.send(main.echanger_mots(dresseur1_mot,dresseur2_mot,dresseur1_trade,dresseur2_trade))
-        else : half_complete=True
+        elif message.content.startswith(f"{prefix}echange ") and message.mentions[0].id!=message.author.id:
+            dresseur1_trade=message.author.id
+            dresseur2_trade=message.mentions[0].id
+            if main.check_dresseur_existe(dresseur2_trade):
+              proposition_trade=True
+              await message.channel.send(f"Le dresseur <@{dresseur1_trade}> propose un **échange** avec le dresseur <@{dresseur2_trade}> ! \
+<@{dresseur2_trade}>, utilisez les commandes `{prefix}accepter` ou `{prefix}refuser`.")
+            else : await message.channel.send(f"Dresseur '{dresseur2_trade}' introuvable.")
+    
+        elif trade_en_cours and message.author.id in [dresseur1_trade,dresseur2_trade] and [dresseur1_mot,dresseur2_mot][[dresseur1_trade,dresseur2_trade].index(message.author.id)]!="" and message.content==f"{prefix}completer":
+            await message.channel.send(f"Le dresseur <@{message.author.id}> bloque le mot '{[dresseur1_mot,dresseur2_mot][[dresseur1_trade,dresseur2_trade].index(message.author.id)]}'.")
+            if half_complete:
+                await message.channel.send(main.echanger_mots(dresseur1_mot,dresseur2_mot,dresseur1_trade,dresseur2_trade))
+            else : half_complete=True
 
-    elif trade_en_cours and message.author.id in [dresseur1_trade,dresseur2_trade]:
-        [dresseur1_mot,dresseur2_mot][[dresseur1_trade,dresseur2_trade].index(message.author.id)]=main.check_mot(mot,message.author.id)
-        if [dresseur1_mot,dresseur2_mot][[dresseur1_trade,dresseur2_trade].index(message.author.id)]=="":
-            await message.channel.send(f"Le dresseur <@{message.author.id}> ne possède pas le mot '{message.content}'. Veuillez rééssayer.")
-        else : await message.channel.send(f"Le dresseur <@{message.author.id}> propose le mot '{message.content}' pour l'échange !")
+        elif trade_en_cours and message.author.id in [dresseur1_trade,dresseur2_trade]:
+            if main.check_mot(message.content,message.author.id)=="":
+                await message.channel.send(f"Le dresseur <@{message.author.id}> ne possède pas le mot '{message.content}'. Veuillez rééssayer.")
+            else :
+              if message.author.id==dresseur1_trade: dresseur1_mot=message.content
+              else: dresseur2_mot=message.content
+              await message.channel.send(f"Le dresseur <@{message.author.id}> propose le mot '{message.content}' pour l'échange !")
+          
+        elif message.content==f"{prefix}quitter":
+          suppression=message.author.id
+          await message.channel.send(f"<@{message.author.id}>, entrez `confirmer` pour confirmer la suppression de votre profil de dresseur, ou `annuler` pour annuler la suppression.")
+      
+        elif message.content=="confirmer" and message.author.id==suppression:
+            await message.channel.send(main.suppression_dresseur(message.author.id))
+      
+        elif message.content=="annuler" and message.author.id==suppression:
+          suppression=""
+          await message.channel.send("Suppression annulée. Ouf !")
 
 keep_alive()
 iencli.run(os.getenv('TOKEN'))
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import requests,sqlite3
 from bs4 import BeautifulSoup
+import datetime
 
 def simp(mot):
     mot=mot.replace("Ã©","é")
@@ -28,7 +29,7 @@ def creer_dresseur(dresseur):
     except sqlite3.Error as error:
         return(f"Error while connecting to sqlite : {error}")
     
-    sql_select_Query = (f"INSERT INTO dresseurs (nom) VALUES ('{dresseur}')")
+    sql_select_Query = (f"INSERT INTO dresseurs (nom,cooldown) VALUES ('{dresseur}',{datetime.datetime.now()-timedelta(hours=20)})")
     cursor = sqliteConnection.cursor()
     cursor.execute(sql_select_Query)
 
@@ -49,7 +50,18 @@ def ouverture_booster(dresseur):
          ]
   return(capturer_mots(liste,dresseur))
 
-
+def cooldown_ready(dresseur):
+    try:
+        sqliteConnection = sqlite3.connect('WordPacks.db')
+    except sqlite3.Error as error:
+        return(f"Error while connecting to sqlite : {error}")
+    cursor = sqliteConnection.cursor()
+    cursor.execute(f"select cooldown from dresseurs WHERE nom='{dresseur}'")
+    cooldown = cursor.fetchall()[0][0]
+    if cooldown<datetime.datetime.now()-timedelta(hours=12):
+      return (True,cooldown<datetime.datetime.now()-timedelta(hours=20))
+    return (False,cooldown<datetime.datetime.now()-timedelta(hours=20))
+  
 #________________________________________________________________________
 
 def capturer_mots(mots,dresseur):
@@ -67,9 +79,9 @@ def capturer_mots(mots,dresseur):
         try:
             cursor = sqliteConnection.cursor()
             cursor.execute(f"INSERT INTO mots (nom,dresseur) VALUES ('{mot_capture}','{id_dresseur}')")
-        except:
+        except sqlite3.IntegrityError:
             cursor = sqliteConnection.cursor()
-            cursor.execute(f"INSERT INTO mots (nom,dresseur) VALUES ('{mot_capture}','{id_dresseur}')")
+            cursor.execute(f"UPDATE mots SET dresseur = '{id_dresseur}' WHERE nom= '{mot_capture}'")
 
     sqliteConnection.commit()
     
@@ -110,10 +122,13 @@ def check_mot(mot,dresseur):
         return(f"Error while connecting to sqlite : {error}")
         return
     cursor = sqliteConnection.cursor()
-    cursor.execute(f"select ID from dresseurs WHERE nom='{dresseur1}'")
+    cursor.execute(f"select ID from dresseurs WHERE nom='{dresseur}'")
     record = cursor.fetchall()
-    cursor.execute(f"select nom from mots WHERE nom='{mot1}' and dresseur='{record[0][0]}'")
+    cursor.execute(f"select nom from mots WHERE nom='{mot}' and dresseur='{record[0][0]}'")
     record = cursor.fetchall()
+    if (sqliteConnection):
+        sqliteConnection.close()
+      
     if record:
         return(record[0][0])
     else:
@@ -137,8 +152,51 @@ def echanger_mots(mot1,mot2,dresseur1,dresseur2):
     if not record2:
         return (f"@{dresseur2} n'a pas le mot '{mot2}'")
     """
-    
+    cursor = sqliteConnection.cursor()
     cursor.execute(f"update mots set dresseur='{dresseur1}' where nom='{mot2}'")
     cursor.execute(f"update mots set dresseur='{dresseur2}' where nom='{mot1}'")
+    sqliteConnection.commit()
+    if (sqliteConnection):
+        sqliteConnection.close()
     return(f"Échange **complété** ! @{dresseur1} possède maintenant '{mot2}', et @{dresseur2} possède maitenant '{mot1}'")
 
+#______________________________________________
+
+def suppression_dresseur(dresseur):
+    try:
+        sqliteConnection = sqlite3.connect('WordPacks.db')
+    except sqlite3.Error as error:
+        return(f"Error while connecting to sqlite : {error}")
+    cursor = sqliteConnection.cursor()
+    cursor.execute(f"select ID from dresseurs WHERE nom='{dresseur}'")
+    record = cursor.fetchall()
+    cursor.execute(f"DELETE from mots WHERE dresseur='{record[0][0]}'")
+    cursor.execute(f"DELETE from dresseurs WHERE ID={record[0][0]}")
+    sqliteConnection.commit()
+    if (sqliteConnection):
+        sqliteConnection.close()
+    return(f"Dresseur <@{dresseur}> supprimé avec succès. On est tristes de vous voir partir !")
+
+#___________________________________________
+
+def check_dresseur_existe(dresseur):
+    try:
+        sqliteConnection = sqlite3.connect('WordPacks.db')
+    except sqlite3.Error as error:
+        return(f"Error while connecting to sqlite : {error}")
+    cursor = sqliteConnection.cursor()
+    cursor.execute(f"select ID from dresseurs WHERE nom='{dresseur}'")
+    record = cursor.fetchall()
+    return not not record
+
+#___________________________________________
+
+def liste_dresseurs():
+    try:
+        sqliteConnection = sqlite3.connect('WordPacks.db')
+    except sqlite3.Error as error:
+        return(f"Error while connecting to sqlite : {error}")
+    cursor = sqliteConnection.cursor()
+    cursor.execute(f"select nom from dresseurs")
+    record = cursor.fetchall()
+    return record
